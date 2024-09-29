207b25cb790cd577cfce528552c7f7b1f58e9f67
RyogoIimura <116061207+RyogoIimura@users.noreply.github.com>
2024-09-29 21:14:21 +0900

    db sqlite に変更

---
+++ prisma/migrations/20240929121014_init/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "tel" TEXT,
+    "address" TEXT,
+    "birth" TEXT,
+    "created_at" DATETIME NOT NULL,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "items" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "category" BIGINT NOT NULL,
+    "price" TEXT NOT NULL,
+    "capacity" TEXT,
+    "maximum_temperature" TEXT,
+    "created_at" DATETIME NOT NULL,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "reservations" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "user_id" TEXT NOT NULL,
+    "items_id" TEXT NOT NULL,
+    "start_time" DATETIME NOT NULL,
+    "end_time" DATETIME NOT NULL,
+    "people_cont" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "reservations_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "reservations_items_id_fkey" FOREIGN KEY ("items_id") REFERENCES "items" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
